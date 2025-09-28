2e65737a880bf1f3acacd24869da676147a72cf9
Hunter McGraw <115018951+huntermcgraw@users.noreply.github.com>
2025-09-28 11:04:44 -0400

    Update reader.c

--- reader.c
+++ reader.c
@@ -21,11 +21,13 @@ int main ()
    int shmId; 
    char *sharedMemoryPtr;
    struct shmid_ds shmid_struct;
+   key_t passkey;
    
    if((shmId = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) { 
       perror ("Unable to get shared memory\n"); 
       exit (1); 
-   } 
+   }
+   passkey = ftok();
    if((sharedMemoryPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
       perror ("Unable to attach\n"); 
       exit (1); 
@@ -49,3 +51,4 @@ int main ()
 
    return 0; 
 }
+
